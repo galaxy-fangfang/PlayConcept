d872e991ab9c354a5429efd3d8abe707241b37c2
alammehwish <alammehw@gmail.com>
2024-05-15 10:53:56 +0200

    prompt-llama2

---
+++ prompt_llama2.py
@@ -0,0 +1,5 @@
+from langchain.llms import Ollama
+ollama = Ollama(base_url='http://localhost:11434', model="llama2")
+#print(ollama("given the main concept 'food' and sub concepts 'animal', 'fly', 'yellow' and 'black' which of the following candidates is most likely the output concept: apple, honey or house. please choose one conept from the list of output concepts. "))
+#print(ollama("Given the main concept 'food' and sub concepts 'animal', 'fly', 'yellow' and 'black' which of the following candidates is most likely the output concept: apple, honey or house. Please choose one conept from the list of output concepts and print in a json format. "))
+print(ollama("Given the main concept 'food' and sub concept 'animal' which of the following candidates is most likely the output concept: apple, honey or house. Please choose one conept from the list of output concepts and print in a json format."))
